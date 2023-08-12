e46843a2e6a209753b86c7e82f759aabeafcadd6
clydeqin7 <clydeqin7@foxmail.com>
2023-08-12 17:25:58 +0800

    refator: 每一步分开赋值

--- src/main/java/com/jirengu/BasicOperator.java
+++ src/main/java/com/jirengu/BasicOperator.java
@@ -10,7 +10,13 @@ public class BasicOperator {
         //Step 5：用100减去上一步的结果，打印剩余的数值
         int num1 = 3;
         int num2 = 14;
-        int result = 100 - ((((num2 * 4) + num1) / 3) % 5);
+        int step1 = num2 * 4;
+        int step2 = num1 + step1;
+        int step3 = step2 / 3;
+        int step4 = step3 % 5;
+        int step5 = 100 - step4;
+//        int result = 100 - ((((num2 * 4) + num1) / 3) % 5);
+        int result = step5;
         System.out.println(result);
     }
 }
